c00b72b814a59f09bcc48c6bed864062601c7ecd
Gary Prat <gary73@gmx.fr>
2020-04-29 18:17:53 +0200

    Ajouts affichage/stat sur colonne+Test unitaire

--- src/main/java/Dataframe.java
+++ src/main/java/Dataframe.java
@@ -24,6 +24,122 @@ public class Dataframe implements Comparable {
 
     }
 
+
+    public void afficher(){
+        String result="";
+        Object[] index= this.data.keySet().toArray();
+        for (Object o:index)
+        {
+            result+=o.toString()+",";
+        }
+        result=result.substring(0,result.length()-1);
+        result+="\n";
+        for (int i=0;i<this.data.get(index[0].toString()).size();i++)
+        {
+            for (int j=0;j<this.data.size();j++)
+            {
+                result+=this.data.get(index[j]).get(i)+",";
+            }
+            result=result.substring(0,result.length()-1);
+            result+="\n";
+
+        }
+        System.out.println(result);
+    }
+
+    public void affichertete(){
+        String result="";
+        int nbligne;
+        Object[] index= this.data.keySet().toArray();
+        for (Object o:index)
+        {
+            result+=o.toString()+",";
+        }
+        result=result.substring(0,result.length()-1);
+        result+="\n";
+        if (this.data.get(index[0].toString()).size()<5)
+            nbligne=this.data.get(index[0].toString()).size();
+        else nbligne=5;
+        for (int i=0;i<nbligne;i++)
+        {
+            for (int j=0;j<this.data.size();j++)
+            {
+                result+=this.data.get(index[j]).get(i)+",";
+            }
+            result=result.substring(0,result.length()-1);
+            result+="\n";
+
+        }
+        System.out.println(result);
+    }
+
+    public void afficherqueue(){
+        String result="";
+        int nbligne;
+        Object[] index= this.data.keySet().toArray();
+        for (Object o:index)
+        {
+            result+=o.toString()+",";
+        }
+        result=result.substring(0,result.length()-1);
+        result+="\n";
+        if (this.data.get(index[0].toString()).size()<5)
+            nbligne=this.data.get(index[0].toString()).size();
+        else nbligne=5;
+        for (int i=this.data.get(index[0].toString()).size()-nbligne;i<this.data.get(index[0].toString()).size();i++)
+        {
+            for (int j=0;j<this.data.size();j++)
+            {
+                result+=this.data.get(index[j]).get(i)+",";
+            }
+            result=result.substring(0,result.length()-1);
+            result+="\n";
+
+        }
+        System.out.println(result);
+    }
+
+
+    public double moyennecollone(String nom)
+    {
+        double moy=0;
+        for (int i=0;i<this.data.get(nom).size();i++) {
+            moy =moy + Double.valueOf(this.data.get(nom).get(i).toString());
+        }
+        moy=moy/this.data.get(nom).size();
+        return moy;
+    }
+
+    public double mincollone(String nom)
+    {
+        double min = Double.MAX_VALUE;
+        for (int i=0;i<this.data.get(nom).size();i++) {
+            if (min>Double.valueOf(this.data.get(nom).get(i).toString()))
+                min=Double.valueOf(this.data.get(nom).get(i).toString());
+        }
+
+        return min;
+    }
+
+    public double maxcollone(String nom)
+    {
+        double max = -Double.MAX_VALUE;
+        for (int i=0;i<this.data.get(nom).size();i++) {
+            if (max<Double.valueOf(this.data.get(nom).get(i).toString()))
+                max=Double.valueOf(this.data.get(nom).get(i).toString());
+
+        }
+
+        return max;
+    }
+
+    @Override
+    public String toString() {
+        return "Dataframe{" +
+                "data=" + data +
+                '}';
+    }
+
     public int compareTo(Object o) {
         Dataframe d =(Dataframe) o;
         if(this.data.size()!=d.data.size())
@@ -34,12 +150,15 @@ public class Dataframe implements Comparable {
         else{
             for(String s:this.data.keySet())
             {
-                for(int i=0;i<this.data.get(s).size();i++)
-                if(this.data.get(s).get(i)!=d.data.get(s).get(i))
-                {
-                    return 0;
+                for(int i=0;i<this.data.get(s).size();i++) {
+                    if(this.data.get(s).size() != d.data.get(s).size())
+                    {
+                        return 0;
+                    }
+                    if (this.data.get(s).get(i) != d.data.get(s).get(i)) {
+                        return 0;
+                    }
                 }
-
             }
             return 1;
         }
--- src/test/java/DataframeTest.java
+++ src/test/java/DataframeTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -17,10 +18,16 @@ class DataframeTest {
         s.add("String1");
         s.add("String2");
         s.add("String3");
+        s.add("String4");
+        s.add("String5");
+        s.add("String6");
         ArrayList<Integer> i =new ArrayList<Integer>();
         i.add(1);
         i.add(2);
         i.add(3);
+        i.add(4);
+        i.add(5);
+        i.add(6);
         HashMap<String,ArrayList> h= new HashMap<String, ArrayList>();
         h.put("0",s);
         h.put("1",i);
@@ -40,10 +47,16 @@ class DataframeTest {
         s.add("String1");
         s.add("String2");
         s.add("String3");
+        s.add("String4");
+        s.add("String5");
+        s.add("String6");
         ArrayList<Integer> i =new ArrayList<Integer>();
         i.add(1);
         i.add(2);
         i.add(3);
+        i.add(4);
+        i.add(5);
+        i.add(6);
         HashMap<String,ArrayList> h= new HashMap<String, ArrayList>();
         h.put("0",s);
         h.put("1",i);
@@ -54,6 +67,29 @@ class DataframeTest {
 
     }
 
+    @Test
+    void MinTest() {
+        Assertions.assertEquals(1,d.mincollone("1"));
+    }
+
+    @Test
+    void MaxTest() {
+        Assertions.assertEquals(6,d.maxcollone("1"));
+    }
+
+    @Test
+    void name() throws IOException {
+
+        d.afficher();
+        d.affichertete();
+        d.afficherqueue();
+        Assertions.assertEquals(0,0);
+    }
+
+    @Test
+    void MoyenneTest() {
+        Assertions.assertEquals(3.5,d.moyennecollone("1"));
+    }
 
     @Test
     void CompareToTrue() {
@@ -61,10 +97,16 @@ class DataframeTest {
         s.add("String1");
         s.add("String2");
         s.add("String3");
+        s.add("String4");
+        s.add("String5");
+        s.add("String6");
         ArrayList<Integer> i =new ArrayList<Integer>();
         i.add(1);
         i.add(2);
         i.add(3);
+        i.add(4);
+        i.add(5);
+        i.add(6);
         HashMap<String,ArrayList> h= new HashMap<String, ArrayList>();
         h.put("0",s);
         h.put("1",i);
